e1f7b695313e056f2f8fac422ea3105160679685
Mateus Tanure <mateusvtt@gmail.com>
2022-09-19 16:00:56 -0300

    flush events when receives 201

--- lib/src/event_buffer.dart
+++ lib/src/event_buffer.dart
@@ -62,6 +62,7 @@ class EventBuffer {
     final status = await client.post(payload);
     switch (status) {
       case 200:
+      case 201:
         await _deleteEvents(eventIds);
         break;
       case 413:
