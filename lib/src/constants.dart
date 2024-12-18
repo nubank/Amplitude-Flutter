1844d2d07ad75065237d11dcc632b90f5bf2eb75
Luis Jaeger <luis.jaeger@nubank.com.br>
2024-12-18 14:20:15 -0300

    handling-db-errors

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   static const packageName = 'amplitude-flutter';
-  static const packageVersion = '1.3.5';
+  static const packageVersion = '1.4.2';
 
   // Local storage
   static const kLocalStoreDeviceIdKey = 'amp:device_id';
