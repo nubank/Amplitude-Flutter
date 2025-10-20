290469fd21ad45348f25e05b2cd42ee2a91a5788
Crdzbird <luisalfonsocb83@gmail.com>
2025-10-20 22:52:17 +0200

    Added copyWith for object copies

--- lib/src/service_provider.dart
+++ lib/src/service_provider.dart
@@ -8,9 +8,10 @@ class ServiceProvider {
     required String apiKey,
     required int timeout,
     required bool getCarrierInfo,
+    Client? client,
     bool enableUuid = true,
     Store? store,
-  })  : client = Client(apiKey),
+  })  : client = client ?? Client(apiKey),
         deviceInfo = DeviceInfo(getCarrierInfo),
         session = Session(timeout),
         store = store ?? Store(enableUuid: enableUuid);
@@ -19,4 +20,21 @@ class ServiceProvider {
   final Store store;
   final Session session;
   final DeviceInfo deviceInfo;
+
+  ServiceProvider copyWith({
+    Client? client,
+    Store? store,
+    Session? session,
+    DeviceInfo? deviceInfo,
+  }) {
+    return ServiceProvider(
+      apiKey: client?.apiKey ?? this.client.apiKey,
+      timeout: session?.timeout ?? this.session.timeout,
+      getCarrierInfo:
+          deviceInfo?.getCarrierInfo ?? this.deviceInfo.getCarrierInfo,
+      client: client ?? this.client,
+      enableUuid: store?.enableUuid ?? this.store.enableUuid,
+      store: store ?? this.store,
+    );
+  }
 }
