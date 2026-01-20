ddfdc3d247945ba6610999ceff7f2370f155ce1d
michele-ribeiro <michele.ribeiro@nubank.com.br>
2026-01-19 21:04:51 -0300

    Update library device_info_plus

--- test/device_info_test.dart
+++ test/device_info_test.dart
@@ -1,38 +1,46 @@
 import 'package:amplitude_flutter/src/constants.dart';
 import 'package:amplitude_flutter/src/device_info.dart';
-import 'package:device_info_platform_interface/device_info_platform_interface.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class MockDeviceInfoPlatform
-    with MockPlatformInterfaceMixin
-    implements DeviceInfoPlatform {
-  @override
-  Future<AndroidDeviceInfo> androidInfo() async =>
-      AndroidDeviceInfo.fromMap(<String, dynamic>{'model': 'android-model'});
-
-  @override
-  Future<IosDeviceInfo> iosInfo() async =>
-      IosDeviceInfo.fromMap(<String, dynamic>{
-        'model': 'ios-model',
-      });
-}
-
 void main() {
-  late DeviceInfo deviceInfo;
+  TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
     SharedPreferences.setMockInitialValues({});
-    DeviceInfoPlatform.instance = MockDeviceInfoPlatform();
-    deviceInfo = DeviceInfo(false);
+
+    // Mock do DeviceInfoPlugin via MethodChannel
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+      const MethodChannel('dev.fluttercommunity.plus/device_info'),
+      (MethodCall methodCall) async {
+        if (methodCall.method == 'getDeviceInfo') {
+          return getDeviceInfo();
+        }
+        return null;
+      },
+    );
+
+    // Mock do PackageInfo
+    PackageInfo.setMockInitialValues(
+      appName: 'Test App',
+      packageName: 'com.test.app',
+      version: '1.0.0',
+      buildNumber: '1',
+      buildSignature: '',
+    );
   });
 
   group('#regenerateDeviceId', () {
     test('when platform is Android', () async {
       debugDefaultTargetPlatformOverride = TargetPlatform.android;
+      final deviceInfo = DeviceInfo(false);
       final platformInfo = (await deviceInfo.getPlatformInfo())!;
+
+      print('[PLATAFORM INFO] $platformInfo');
       expect(
           platformInfo,
           allOf(
@@ -56,4 +64,43 @@ void main() {
       expect(savedDeviceId, equals(newDeviceId));
     });
   });
+}
+
+Map<String, dynamic> getDeviceInfo() {
+  return {
+    'version': {
+      'sdkInt': 20,
+      'codename': 'test',
+      'incremental': 'test',
+      'release': 'test',
+      'previewSdkInt': 20,
+    },
+    'board': 'test',
+    'bootloader': 'test',
+    'brand': 'test',
+    'device': 'test',
+    'display': 'test',
+    'fingerprint': 'test',
+    'hardware': 'test',
+    'host': 'test',
+    'id': 'test',
+    'manufacturer': 'test',
+    'model': 'android-model',
+    'product': 'test',
+    'tags': 'test',
+    'type': 'test',
+    'isPhysicalDevice': true,
+    'serialNumber': 'test',
+    'isLowRamDevice': false,
+    'freeDiskSize': 1000000,
+    'totalDiskSize': 100000,
+    'physicalRamSize': 10000,
+    'availableRamSize': 10000,
+    'displayMetrics': {
+      'widthPx': 100.0,
+      'heightPx': 100.0,
+      'xDpi': 100.0,
+      'yDpi': 100.0,
+    }
+  };
 }
